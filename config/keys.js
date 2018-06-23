de0fbc2031d2778e23e6283b970bca5624fd820f
Astoryed <astoryed@gmail.com>
2018-06-23 21:51:27 +0500

    Now login and register system works

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://astro:astro3000@ds117061.mlab.com:17061/mernlearn'
+    mongoURI: 'mongodb://astro:astro3000@ds117061.mlab.com:17061/mernlearn',
+    secretOrKey: 'secret'
 };
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/user');
 const profiles = require('./routes/api/profile');
@@ -7,6 +9,10 @@ const posts = require('./routes/api/posts');
 
 
 const app = express();
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -17,7 +23,12 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+//Passport Middleware
+app.use(passport.initialize());
+
+//Passport Config
+require('./config/passport')(passport);
+
 
 //Use Routes
 app.use('/api/users', users);
